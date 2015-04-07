c5144b856f1aea69f55f3f998ad599bb68663f18
Chris Archibald <carchi8py@gmail.com>
2015-04-06 20:00:58 -0700

    added name and role to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,10 @@
-$("#main").append(["Chris Archibald"]);
+
+var name = "Chris Archibald";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+$("#header").append(formattedName);
+
+var role = "Software Engineer"
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+
+$("#header").append(formattedRole);

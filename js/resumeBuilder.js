e1ca1d018f706cb5805c12b84916fae69ff4bc03
Chris Archibald <carchi8py@gmail.com>
2015-04-11 18:39:04 -0700

    Starting over. All elements are valid Json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,25 +1,23 @@
 
-var bio  = {
+var bio = {
 	"name" : "Chris Archibald",
-	"role" : "Software Engineer",
-	"contact" : {
-		"mobile"  : "650-504-5125",
+	"role" : "Software Developer",
+	"contacts " : {
+		"mobile" : "650-504-5125",
 		"email" : "carchi8py@gmail.com",
 		"github" : "carchi8py",
-		"location" : "Silicon Valley"
+		"twitter" : "ogrebears",
+		"location" : "Sunnyvale"
 	},
-	"picture" : "images/me.jpg",
-	"message" : "This is a message",
-	"skills" : ["Python", "World Domination", "Java", "Swift"],
-	"bioPic" : "images/fry.jpg"
+	"welcomeMessage" : "This is my welcomeMessage",
+	"skills" : [
+		"Python",
+		"swift",
+		"World Domination",
+		"ice cream"
+	],
+	"bioPic" : "images/fry.jpg" 
 }
-
-var work = {};
-work.employer = "NetApp";
-work.title = "MTS 3";
-work.years = "2008-2015";
-work.city = "Sunnyvale";
-work.Description = "I did stuff";
 
 var education = {
 	"schools" : [
@@ -27,8 +25,7 @@ var education = {
 			"name" : "San Jose State University",
 			"location" : "San Jose",
 			"degree" : "BS",
-			"major" : "Computer Science",
-			"minor" : "Math",
+			"major" : ["Computer Science"],
 			"dates" : "2008",
 			"url" : "sjsu.edu"
 		}
@@ -49,45 +46,42 @@ var education = {
 	]
 }
 
+var work = {
+	"jobs" : [
+		{
+			"employer" : "NetApp",
+			"title" : "MTS 3",
+			"location" : "Sunnyvale",
+			"dates" : "2013-Present"
+		},
+		{
+			"employer" : "NetApp",
+			"title" : "MTS 2",
+			"location" : "Sunnyvale",
+			"dates" : "2010-2013"
+		},
+		{
+			"employer" : "NetApp",
+			"title" : "MTS 1",
+			"location" : "Sunnyvale",
+			"dates" : "2008-2010"
+		}
+	]
+}
 
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedMobile = HTMLmobile.replace("%data%", bio.contact.mobile);
-var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
-var formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
-var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-var formattedHTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.message);
-var formattedHTMLskills = HTMLskills.replace("%data%", bio.skills[0]);
-var formattedHTMLskills1 = HTMLskills.replace("%data%", bio.skills[1]);
-var formattedHTMLskills2 = HTMLskills.replace("%data%", bio.skills[2]);
-var formattedHTMLskills3 = HTMLskills.replace("%data%", bio.skills[3]);
-
-var formattedEmployer = HTMLworkEmployer.replace("%data%", work.employer);
-var formattedTitle = HTMLworkTitle.replace("%data%", work.title);
-var formattedDates = HTMLworkDates.replace("%data%", work.years);
-var formattedCity = HTMLworkLocation.replace("%data%", work.city);
-var formattedDescription = HTMLworkDescription.replace("%data%", work.Description);
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-$("#topContacts").append(formattedMobile);
-$("#topContacts").append(formattedEmail);
-$("#topContacts").append(formattedGithub);
-$("#topContacts").append(formattedLocation);
-
-$("#header").append(formattedBioPic);
-$("#header").append(formattedHTMLwelcomeMsg);
-$("#header").append(HTMLskillsStart);
-$("#skills").append(formattedHTMLskills);
-$("#skills").append(formattedHTMLskills1);
-$("#skills").append(formattedHTMLskills2);
-$("#skills").append(formattedHTMLskills3);
-
-$("#workExperience").append(HTMLworkStart);
-$(".work-entry").append(formattedEmployer);
-$(".work-entry").append(formattedTitle);
-$(".work-entry").append(formattedDates);
-$(".work-entry").append(formattedCity);
-$(".work-entry").append(formattedDescription);
+var projects = {
+	"projects" : [
+		{
+			"title" : "project 1",
+			"Dates" : "somedate",
+			"description" : "This was a project",
+			"images" : "images/fry.jpg"
+		},
+		{
+			"title" : "project 2",
+			"Dates" : "somedate",
+			"description" : "This was a project",
+			"images" : "images/fry.jpg"
+		}
+	]
+}

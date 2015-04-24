608af854266038d47b3a1dcb2e4efb531971f5f4
Chris Archibald <carchi8py@gmail.com>
2015-04-23 20:40:15 -0700

    Added internationalize button

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -213,4 +213,16 @@ work.display();
 projects.display();
 education.display();
 
+function inName(name) {
+    var name = bio.name;
+    name = name.trim().split(" ");
+    console.log(name);
+    name[1] = name[1].toUpperCase();
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+    return name[0] +" "+ name[1];
+}
+
+$("#main").append(internationalizeButton);
+
 $("#mapDiv").append(googleMap);
